f92f6be1947d6713758fb6f9e6d17ca6a5dbf98b
Kyle Carbone <kcarbone@me.com>
2021-06-21 22:19:42 +0100

    basic update and sleep functionality

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = require('http').Server(app);
 const log = require('node-consolog').getLogger('test');
 const oled = require('pi-gadgets').oled;
 const port = process.env.PORT || 7777;
-let display;
+let display, sleepRef;
 
 app.use(express.json());
 
@@ -14,30 +14,59 @@ app.get('/status', (req, res) => {
 });
 
 app.put('/update', async (req, res) => {
-  display.buffer.fill(0);
-
   if(typeof req.body === 'object'){
     const items = Array.isArray(req.body) ? req.body : [req.body];
+    display.clear();
+
     for(let i=0; i<items.length; i++){
       const item = items[i];
-      display.writeText(item.x, item.y, item.text);
-    }
-  }
+      const font = (typeof item.font === 'string') ? item.font : 'small_6x8';
+      const color = (typeof item.color === 'number') ? item.color : 1;
 
-  await display.update();
-  res.status(201).end();
+      if(item.x && item.y && item.text){
+        display.writeText(item.x, item.y, item.text, font, color);
+      }
+      else if(item.x && item.y && item.x2 && item.y2){
+        display.writeRect(item.x, item.y, item.x2, item.y2, color);
+      }
+      else if(item.x && item.y){
+        display.writePixel(item.x, item.y, color);
+      }
+      else if(item.x){
+        display.writeVLine(item.x, color);
+      }
+      else if(item.y){
+        display.writeHLine(item.y, color);
+      }
+      else{
+        log.warn(`Invalid drawing directive: ${JSON.stringify(item)}`);
+      }
+    }
+    await display.update();
+    startSleepTimer(60);
+    res.status(204).end();
+  }
+  else{
+    res.status(400).send('Invalid request');
+  }
 });
+
+function startSleepTimer(timeoutSecs){
+  clearTimeout(sleepRef);
+  sleepRef = setTimeout(() => {
+    display.clear();
+    display.update();
+  }, (timeoutSecs * 1000));
+}
 
 server.listen(port, async callback => {
   display = await oled.init(128, 32);
-  display.writeText(0, 0, 'Ready', oled.fonts.small_8x12);
-  await display.update();
   log.info(`Server running on port ${port}`);
 });
 
 // Termination event
 process.on('SIGINT', function () {
   log.warn('Service shutting down!');
-
-  setTimeout(() => process.exit(), 100);
+  display.clear();
+  display.update().then(() => process.exit());
 });
